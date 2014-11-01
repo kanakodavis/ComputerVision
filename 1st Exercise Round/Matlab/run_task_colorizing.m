8e940df69930a41f6bf388ca87bcf0f61402885f
Robin Melan <el_argentino23@hotmail.com>
2014-11-01 17:07:42 +0100

    Changed run_task_colorizing to save images

--- 1st Exercise Round/Matlab/run_task_colorizing.m
+++ 1st Exercise Round/Matlab/run_task_colorizing.m
@@ -12,7 +12,7 @@ imageNames = {'00125v','00149v','00153v','00351v','00398v','01112v'};
 SearchSize = 15;
 
 %for debug: TODO delete
-%imageNames = {'01112v'};
+% tmp = {'00125v'};
 
 for imageName = imageNames
     %% load images
@@ -20,37 +20,53 @@ for imageName = imageNames
     [R, G, B] = LoadRGBGlassPlateScans(imageName);
     
     %% align images automatically
-    
+    % Take time
+    tic;
     
     G_aligned = FasterAlignWithNCCMetric(R,G,SearchSize);
     B_aligned = FasterAlignWithNCCMetric(R,B,SearchSize);
     R_aligned = R;
     
+    elapsedTimeFast = num2str(toc);
+    
     %% combine and display them 
     
-    RGB(:,:,1) = R_aligned;
-    RGB(:,:,2) = G_aligned;
-    RGB(:,:,3) = B_aligned;
+    RGB = cat(3, R_aligned, G_aligned, B_aligned);
+    %RGB(:,:,1) = R_aligned;
+    %RGB(:,:,2) = G_aligned;
+    %RGB(:,:,3) = B_aligned;
     figure;
     imshow(RGB);
     
     %% align images automatically
+    % Take time
+     tic
     
-%     BestMatchCircShift = AlignWithNCCMetric(R,G,SearchSize);
-%    	G_aligned = circshift( G , BestMatchCircShift );
+     BestMatchCircShift = AlignWithNCCMetric(R,G,SearchSize);
+    	G_aligned = circshift( G , BestMatchCircShift );
+     
+     BestMatchCircShift = AlignWithNCCMetric(R,B,SearchSize);
+    	B_aligned = circshift( B , BestMatchCircShift );
 %     
-%     BestMatchCircShift = AlignWithNCCMetric(R,B,SearchSize);
-%    	B_aligned = circshift( B , BestMatchCircShift );
-%     
-%     R_aligned = R;
-%     
+     R_aligned = R;
+     
+     elapsedTimeSimple = num2str(toc);
+     
 %     %% combine and display them 
-%     
-%     RGB(:,:,1) = R_aligned;
-%     RGB(:,:,2) = G_aligned;
-%     RGB(:,:,3) = B_aligned;
+%    
+     RGB2 = cat(3, R_aligned, G_aligned, B_aligned);
+     %RGB2(:,:,1) = R_aligned;
+     %RGB2(:,:,2) = G_aligned;
+     %RGB2(:,:,3) = B_aligned;
 %     figure;
-%     imshow(RGB);
+%     imshow(RGB2);
+     
+     %% Save new Image with and without Correlation for the Report
+          
+     imageWithCorrBonus = RGB;
+     imageWithCorr      = RGB2;
+     imageWithoutCorr   = cat(3, R, G, B);
+     SaveRGBImagesForReport(imageName, imageWithoutCorr, imageWithCorr, imageWithCorrBonus, elapsedTimeSimple, elapsedTimeFast);
     
 end
 
