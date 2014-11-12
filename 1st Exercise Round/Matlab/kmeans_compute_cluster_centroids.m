a04be77e0217385dd1dad24a51b57de67970c58b
Robin Melan <el_argentino23@hotmail.com>
2014-11-12 15:24:38 +0100

    better performance for segmentation exercise

--- 1st Exercise Round/Matlab/kmeans_compute_cluster_centroids.m
+++ 1st Exercise Round/Matlab/kmeans_compute_cluster_centroids.m
@@ -23,23 +23,35 @@ function [u] = kmeans_compute_cluster_centroids(X, k , r, u)
         tmp(r==i) = 1;
         tmp(r~=i) = 0;
         nenner = sum(tmp);
+        
+        % Cannot devide by 0!
         if (nenner ~= 0)
             % Zähler
             tmp = zeros(size(r));
             tmp(r==i) = 1;
-            zaehler = [0 0 0];
-            for pixel = 1:size(X,1)
-                % Multiply only if pixel is relevant
-                if (tmp(pixel,1) ~= 0)
-                    zaehler(1) = zaehler(1) + tmp(pixel,1) * X(pixel,1);
-                    zaehler(2) = zaehler(2) + tmp(pixel,1) * X(pixel,2);
-                    zaehler(3) = zaehler(3) + tmp(pixel,1) * X(pixel,3);
-                end
-            end
+%             zaehler = [0 0 0];
+            
+            % Better Performance:
+            X_col1 = X(:,1);
+            X_col2 = X(:,2);
+            X_col3 = X(:,3);
+            
+            u(i,1) = sum(tmp .* X_col1) / nenner;
+            u(i,2) = sum(tmp .* X_col2) / nenner;
+            u(i,3) = sum(tmp .* X_col3) / nenner;
+            
+%             for pixel = 1:size(X,1)
+%                 % Multiply only if pixel is relevant
+%                 if (tmp(pixel,1) ~= 0)
+%                     zaehler(1) = zaehler(1) + tmp(pixel,1) * X(pixel,1);
+%                     zaehler(2) = zaehler(2) + tmp(pixel,1) * X(pixel,2);
+%                     zaehler(3) = zaehler(3) + tmp(pixel,1) * X(pixel,3);
+%                 end
+%             end
 
-            u(i,1) = zaehler(1) / nenner;
-            u(i,2) = zaehler(2) / nenner;
-            u(i,3) = zaehler(3) / nenner;
+%             u(i,1) = zaehler(1) / nenner;
+%             u(i,2) = zaehler(2) / nenner;
+%             u(i,3) = zaehler(3) / nenner;
         end
     end
 
