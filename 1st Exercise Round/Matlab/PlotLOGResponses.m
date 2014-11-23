bcb68bea99dc9d3eb87d86c3db802fea074c5099
Matthias Gusenbauer <matthias.gusenbauer@me.com>
2014-11-23 03:31:27 +0100

    final si blob detection push

--- 1st Exercise Round/Matlab/PlotLOGResponses.m
+++ 1st Exercise Round/Matlab/PlotLOGResponses.m
@@ -6,7 +6,10 @@ figure
 hold on
 stem(scales, smallResp, 'filled');
 stem(scales, bigResp, 'r', 'filled');
+legend('Image orig. size','Image half size','Location','northwest')
 xlim([1.5 max(scales)+0.5]);
+xlabel('LoG size');
+ylabel('LoG response');
 hold off
 
 end
