514d160f2daf9f8026bdcf8257bb6389410734e3
Matthias Gusenbauer <matthias.gusenbauer@me.com>
2014-12-14 23:10:05 +0100

    [FEATURE] improved assignement structure

--- 2nd Exercise Round/Matlab/run_task_image_stitching.m
+++ 2nd Exercise Round/Matlab/run_task_image_stitching.m
@@ -1,6 +1,22 @@
 function run_task_image_stitching()
 %run_task_image_stitching Performing image stitching on given images
 
+ImageNames = {'Material/campus','Material/officeview'};
+ImageFileType = '.jpg';
+
+for imageName = ImageNames
+    
+    %Create image names and load images
+    ImNames = cell(5, 1);
+    Images = cell(1, 5);
+    for i=1:5
+        ImNames(i) = strcat(strcat(imageName, num2str(i)), ImageFileType);
+        Images{i} = imread(ImNames{i});
+    end
+    
+    %Do image processing
+    
+end
 
 end
 
